077a084d5574a302fae5d15e35da6b17474d967d
KestutisVaitiekunas <kestutis.vaitiekunas@stud.ktmc.lt>
2023-10-31 20:44:17 +0200

    sutvarkiau baneri,input,main nav.added script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 
 let naujienlaiskis = document.getElementById("e-mail");
-naujienlaiskis.addEventListener("click", () => naujienlaiskis.value = "");
+naujienlaiskis.addEventListener("click", () => naujienlaiskis.value = "");// paspaudus ant laukelio isnuksta tekstas, rasoma is naujo
 
 document.getElementsByClassName("pre_footer_button")[0].addEventListener("click", tikrintiEmaila);
 function tikrintiEmaila(){
-    let txt = document.getElementById("e-mail").value;
-    console.log(txt)
+    let regex = /.+(\W)?.*@\w+\.\w+/ig// regex sablonas tikrinti el pasto strukturai
+    let txt = document.getElementById("e-mail");
+    if (regex.test(txt.value)){ // jei ivestas tekstas atitinka regex sablona tada el pasto adresasa suformuluotas teisingai
+        //cia bus kodas siunciantis al pasto adresa i duombaze
+        console.log("El. pastas tinka")
+    }else{
+        document.getElementById("e-mail").style.backgroundColor = "#fad3c8"// jei neteisingas ,nuspalvinu laukeli rausvai (klaida!).
+        txt.value = "Įveskite tinkamą el. pašto adresą !"
+    }   
 }
+// mygtuku eventai karuselei. pastumia kairen ir desinen.
+document.getElementById("scroll_right").addEventListener("click", function(){
+    document.getElementById("slider").scrollBy({
+        left: 1,
+        behavior: "smooth"
+    })  
+})
+document.getElementById("scroll_left").addEventListener("click", function(){
+    document.getElementById("slider").scrollBy({
+        left: -1,
+        behavior: "smooth"
+    })  
+})
+
